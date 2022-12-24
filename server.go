3a4fabdec30980c0e4948a7d6c72f66704336338
Sutee Saraphan <suteesaraphan27@gmail.com>
2022-12-24 23:05:16 +0700

    refac, initDB, handleware

--- server.go
+++ server.go
@@ -1,28 +1,60 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-const defaultPort = ":2565"
+func initDB() *sql.DB {
+	dbStr := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", dbStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
+
+const defaultPort = "2565"
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello Welcome to the server KKGO:assessment")
+}
 
 func main() {
+
+	db := initDB()
+
 	e := echo.New()
-	e.GET("/", func(s echo.Context) error {
-		return s.String(200, "Hello")
-	})
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+
+	//use middleware to log requests
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			e.Logger.Printf("Request: %s %s %s", c.Request().Method, c.Request().URL, c.Request().RemoteAddr)
+			return next(c)
+		}
+	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	e.Logger.Fatal(e.Start(port))
+	err := e.Start(":" + port)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
 
 }
