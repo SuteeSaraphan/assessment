0ac7e3ce5421b1e7dcdbf86857cfb7cfd7592521
Sutee Saraphan <suteesaraphan27@gmail.com>
2022-12-24 22:30:47 +0700

    gomod & server run port 2565

--- server.go
+++ server.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
+const defaultPort = ":2565"
+
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	e := echo.New()
+	e.GET("/", func(s echo.Context) error {
+		return s.String(200, "Hello")
+	})
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	e.Logger.Fatal(e.Start(port))
+
 }
